698324107106ce3b77498d586a1684ef473a831b
Maliq Adewale <maliqade@udel.edu>
2024-04-04 17:16:48 -0400

    started working on adding images

---
+++ image-component/image.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { Button, Row, Col } from "react-bootstrap";
+
+export function ChangeImage(): JSX.Element{
+    type Images = " https://shorturl.at/lmrC9" | "https://shorturl.at/hzHQ7" | "https://shorturl.at/ahoCO" | 
+    "https://shorturl.at/lpzN2" | " https://rb.gy/64cext"
+    return(
+        <div></div>
+
+    );
+}
