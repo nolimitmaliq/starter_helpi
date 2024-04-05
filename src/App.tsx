111eff917822bf6d01aff61dfac6418511ddbbc6
Rahul <rahulp@udel.edu>
2024-04-05 13:47:38 -0400

    I still need to create a better outline and add the new pages

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://Careerquizlo.org"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -49,13 +49,38 @@ function App() {
       </header>
 
       <header className="App-buttons">
-        <a href="https://reactjs.org/Basic"  target="_blank">
+        <h1>Career Quizzes</h1>
+        <h3>Have you ever wondered what you wanted to do in life, but could never find a passionate career?
+          Well, now you can, with the Basic Quiz and Detailed Quiz, you can figure out your future. Anyone, from
+          any age, race, ethnicity, sex, and nationality can take this quiz.
+        </h3>
+        <body>
+        <header className="App-header2">
+          <h1>Basic Quiz</h1>
+        </header>
+          <p>Basic Quiz offers a "Check All That Apply" questions. This allows the user
+            to choose multiple answers for each question to generate a more accurate career result.
+          </p>
+        </body>
+        <a href="https://Careerquizlo.org/Basic"  target="_blank" rel="noreferrer">
           <button className="button-style">Take Quiz</button>
         </a>
-        <a href="https://reactjs.org/Basic"  target="_blank">
+        <header className="App-header2">
+          <h1>Detailed Quiz</h1>
+        </header>
+          <p>Detailed Quiz offers fill in the blank questions. This allows the user
+            to answer with their own freedom for each question to generate a more accurate career result.
+          </p>
+        <a href="https://Careerquizlo.org/Detailed"  target="_blank" rel="noreferrer">
           <button className="button-style">Take Quiz</button>
         </a>
-        <a href="https://reactjs.org/Basic"  target="_blank" rel="noreferrer">
+        <header className="App-header2">
+          <h1>Career Tips</h1>
+        </header>
+          <p>Career Tips allows users to learn how to get thier potential career. This page will
+            enlighten users about useful sites and the hiring process.
+          </p>
+        <a href="https://Careerquizlo.org/Tips"  target="_blank" rel="noreferrer">
           <button className="button-style">View Tips</button>
         </a>
       </header>
