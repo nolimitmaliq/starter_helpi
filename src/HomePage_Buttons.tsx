08f67138039661f795bb360b616dd1de78953f6f
Maliq Adewale <maliqade@udel.edu>
2024-04-07 14:51:21 -0400

    adding HomeButton file

--- src/HomePage_Buttons.tsx
+++ src/HomePage_Buttons.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Button, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 import { FaArrowRight } from "react-icons/fa";
 
