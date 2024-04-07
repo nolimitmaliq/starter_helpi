64e78e65c669423c2fc8219a648feeb7ad82d360
Maliq Adewale <maliqade@udel.edu>
2024-04-07 15:57:09 -0400

    last time

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 // import React, { useState } from "react";
 import "./App.css";
 // import { Button, Form } from "react-bootstrap";
 // import { ChangeImages } from "./image2";
 import { HomePage } from "./HomePage_Buttons";
 
-//local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
+// local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 // let keyData = "";
 // const saveKeyData = "MYKEY";
 // const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
@@ -13,15 +12,14 @@ import { HomePage } from "./HomePage_Buttons";
 //   keyData = JSON.parse(prevKey);
 // }
 
-function openNewWindow() {
-  window.open("https://careerquizlo.org", "_blank", "width=600,height=400");
-}
+// function openNewWindow() {
+//   window.open("https://careerquizlo.org", "_blank", "width=600,height=400");
+// }
 
 function App() {
-
   // const [key, setKey] = useState<string>(keyData); //for api key input
 
-  // // sets the local storage item to the api key the user inputed
+  // sets the local storage item to the api key the user inputed
   // function handleSubmit() {
   //   localStorage.setItem(saveKeyData, JSON.stringify(key));
   //   window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
