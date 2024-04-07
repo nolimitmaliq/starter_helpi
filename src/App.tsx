ac5816a274937f4d0919ad64757429c46b8c7fa7
Maliq Adewale <maliqade@udel.edu>
2024-04-07 14:49:14 -0400

    HomePage_Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Button, Form } from "react-bootstrap";
-import { ChangeImages } from "./image2";
+// import { Button, Form } from "react-bootstrap";
+// import { ChangeImages } from "./image2";
 import { HomePage } from "./HomePage_Buttons";
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
@@ -15,7 +15,7 @@ if (prevKey !== null) {
 function App() {
   const [key, setKey] = useState<string>(keyData); //for api key input
 
-  //sets the local storage item to the api key the user inputed
+  // sets the local storage item to the api key the user inputed
   function handleSubmit() {
     localStorage.setItem(saveKeyData, JSON.stringify(key));
     window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
