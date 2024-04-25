332685fea57063980f496a6f833f6cd4cc450e95
Rahul <rahulp@udel.edu>
2024-04-25 14:11:11 -0400

    Having issues with local and session storage

--- src/BasicQuiz.tsx
+++ src/BasicQuiz.tsx
@@ -127,12 +127,12 @@ let questions: Question[] = [
 ];
 //LOCALSTORAGE
 const quizKey = "quiz";
-const previousData = localStorage.getItem(quizKey);
+const previousData = sessionStorage.getItem(quizKey);
 if (previousData !== null) {
   questions = JSON.parse(previousData);
 }
 
-function Quiz() {
+export function BasicQuestion() {
   const [tab, setTab] = useState<string>("basic");
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedOptions, setSelectedOptions] = useState<string[]>(
@@ -148,13 +148,10 @@ function Quiz() {
   const PrevQuestion = () => {
     setCurrentQuestion(currentQuestion - 1);
   };
-  const Submit = () => {
-    if (currentQuestion === 10) {
-      setVisible(!visible);
-    }
-    setTab("detailed");
+  function Submit() {
     saveData();
-  };
+    setTab("detailed");
+  }
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newAnswer = [...selectedOptions];
     newAnswer[currentQuestion] = e.target.value;
@@ -175,7 +172,7 @@ function Quiz() {
     }
   };
   function saveData() {
-    localStorage.setItem(quizKey, JSON.parse(selectedOptions[currentQuestion]));
+    sessionStorage.setItem(quizKey, JSON.stringify(selectedOptions));
   }
   const [progress, setProgress] = useState(0);
   const handleNextClick = () => {
@@ -209,141 +206,6 @@ function Quiz() {
 
   return (
     <>
-      {tab === "detailed" ? (
-        <DetailedQuestion />
-      ) : (
-        <>
-          <div
-            style={{
-              height: "33rem",
-              margin: " -1.4rem auto",
-            }}
-          >
-            <div className="container">
-              <div className="progress-bar">
-                <div
-                  className="progress-bar-fill"
-                  style={{ width: `${progress}%`, backgroundColor: getColor() }}
-                ></div>
-              </div>
-              <div className="progress-label">{progress}%</div>
-            </div>
-            <div className="StaticBackground">
-              <h1>{questions[currentQuestion].question}</h1>
-              {questions[currentQuestion].type === "multipleChoice" && (
-                <div>
-                  {questions[currentQuestion].options.map((option) => (
-                    <Form.Check
-                      key={option}
-                      type="radio"
-                      id={option}
-                      name="choices"
-                      value={option}
-                      onChange={handleInputChange}
-                      label={option}
-                      checked={selectedOptions[currentQuestion] === option}
-                    ></Form.Check>
-                  ))}
-                </div>
-              )}
-              {questions[currentQuestion].type === "checkbox" && (
-                <div>
-                  {questions[currentQuestion].options.map((option) => (
-                    <Form.Check
-                      key={option}
-                      type="checkbox"
-                      id={option}
-                      name="choices-chsckbox"
-                      value={option}
-                      onChange={handleInputChange2}
-                      label={option}
-                      checked={selectedOptions[currentQuestion].includes(
-                        option
-                      )}
-                    ></Form.Check>
-                  ))}
-                </div>
-              )}
-              {questions[currentQuestion].type === "textbox" && (
-                <Form.Group controlId={`Question-${currentQuestion}`}>
-                  <Form.Label
-                    style={{
-                      display: "block",
-                      marginBottom: "10px",
-                      color: "white",
-                      fontSize: "25px",
-                      textAlign: "center",
-                      margin: "0 auto",
-                      maxWidth: "80%",
-                    }}
-                  >
-                    {questions[currentQuestion].options}
-                  </Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    value={selectedOptions[currentQuestion]}
-                    onChange={handleInputChange}
-                    style={{
-                      margin: "20px auto",
-                      height: "200px",
-                      width: "700px",
-                      border: "1px solid gray",
-                      background: "black",
-                      color: "white",
-                      fontSize: "17px",
-                    }}
-                  />
-                </Form.Group>
-              )}
-            </div>
-            <div className="ButtonSpace">
-              <Button
-                onClick={handlePrev}
-                disabled={currentQuestion === 0}
-                className="button"
-                style={{
-                  right: "20px",
-                }}
-              >
-                Previous
-              </Button>
-              {currentQuestion === questions.length - 1 ? (
-                <Button
-                  onClick={Submit}
-                  className="button"
-                  style={{
-                    left: "20px",
-                    margin: "24px auto",
-                  }}
-                >
-                  Submit
-                </Button>
-              ) : (
-                <Button
-                  onClick={handleNext}
-                  disabled={currentQuestion === questions.length - 1}
-                  className="button"
-                  style={{
-                    left: "20px",
-                    margin: "24px auto",
-                  }}
-                >
-                  Next
-                </Button>
-              )}
-            </div>
-          </div>
-        </>
-      )}
-    </>
-  );
-}
-
-export function BasicQuestion() {
-  const [tab, setTab] = useState<string>("basic");
-
-  return (
-    <>
       {tab === "home" ? (
         <HomePage />
       ) : tab === "detailed" ? (
@@ -369,7 +231,132 @@ export function BasicQuestion() {
               </div>
             </div>
             <div className="QuizBody">
-              <Quiz />
+              <div
+                style={{
+                  height: "33rem",
+                  margin: " -1.4rem auto",
+                }}
+              >
+                <div className="container">
+                  <div className="progress-bar">
+                    <div
+                      className="progress-bar-fill"
+                      style={{
+                        width: `${progress}%`,
+                        backgroundColor: getColor(),
+                      }}
+                    ></div>
+                  </div>
+                  <div className="progress-label">{progress}%</div>
+                </div>
+                <div className="StaticBackground">
+                  <h1>{questions[currentQuestion].question}</h1>
+                  {questions[currentQuestion].type === "multipleChoice" && (
+                    <div>
+                      {questions[currentQuestion].options.map((option) => (
+                        <Form.Check
+                          key={option}
+                          type="radio"
+                          id={option}
+                          name="choices"
+                          value={option}
+                          onChange={handleInputChange}
+                          label={option}
+                          checked={selectedOptions[currentQuestion] === option}
+                        ></Form.Check>
+                      ))}
+                    </div>
+                  )}
+                  {questions[currentQuestion].type === "checkbox" && (
+                    <div>
+                      {questions[currentQuestion].options.map((option) => (
+                        <Form.Check
+                          key={option}
+                          type="checkbox"
+                          id={option}
+                          name="choices-chsckbox"
+                          value={option}
+                          onChange={handleInputChange2}
+                          label={option}
+                          checked={selectedOptions[currentQuestion].includes(
+                            option
+                          )}
+                        ></Form.Check>
+                      ))}
+                    </div>
+                  )}
+                  {questions[currentQuestion].type === "textbox" && (
+                    <Form.Group controlId={`Question-${currentQuestion}`}>
+                      <Form.Label
+                        style={{
+                          display: "block",
+                          marginBottom: "10px",
+                          color: "white",
+                          fontSize: "25px",
+                          textAlign: "center",
+                          margin: "0 auto",
+                          maxWidth: "80%",
+                        }}
+                      >
+                        {questions[currentQuestion].options}
+                      </Form.Label>
+                      <Form.Control
+                        as="textarea"
+                        value={selectedOptions[currentQuestion]}
+                        onChange={handleInputChange}
+                        style={{
+                          margin: "20px auto",
+                          height: "200px",
+                          width: "700px",
+                          border: "1px solid gray",
+                          background: "black",
+                          color: "white",
+                          fontSize: "17px",
+                        }}
+                      />
+                    </Form.Group>
+                  )}
+                </div>
+                <div className="ButtonSpace">
+                  <Button
+                    onClick={handlePrev}
+                    disabled={currentQuestion === 0}
+                    className="button"
+                    style={{
+                      right: "20px",
+                    }}
+                  >
+                    Previous
+                  </Button>
+                  {currentQuestion === questions.length - 1 ? (
+                    <Button
+                      onClick={() => {
+                        setTab("detailed");
+                        saveData();
+                      }}
+                      className="button"
+                      style={{
+                        left: "20px",
+                        margin: "24px auto",
+                      }}
+                    >
+                      Submit
+                    </Button>
+                  ) : (
+                    <Button
+                      onClick={handleNext}
+                      disabled={currentQuestion === questions.length - 1}
+                      className="button"
+                      style={{
+                        left: "20px",
+                        margin: "24px auto",
+                      }}
+                    >
+                      Next
+                    </Button>
+                  )}
+                </div>
+              </div>
             </div>
             {/* <div className="QuizBody">
               <Button onClick={() => setTab("detailed")} className="button">
