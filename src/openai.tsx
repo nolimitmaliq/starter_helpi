4f16147ab9bcfcc8f28da82606f485402605775e
Rahul <rahulp@udel.edu>
2024-04-26 13:11:59 -0400

    local storage probably works

--- src/openai.tsx
+++ src/openai.tsx
@@ -1,19 +1,19 @@
-// import { OpenAI } from "openai";
+import { OpenAI } from "openai";
+let API_KEY = "one";
+const openai = new OpenAI({ apiKey: API_KEY, dangerouslyAllowBrowser: true });
 
-// const openai = new OpenAI({ apiKey: API_KEY, dangerouslyAllowBrowser: true });
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "We constructed a set of questions, multiple choice, checkbox, short responses and long respones. Our goal is to try and provide 5 possible career paths based on the user responses to the questions with an explaination about the careers including their salary, possible path to their career, an indeed link and a linkdIn link to potential jobs and potential networking oppurtunities.",
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
 
-// async function main() {
-//   const completion = await openai.chat.completions.create({
-//     messages: [
-//       {
-//         role: "system",
-//         content:
-//           "We constructed a set of questions, multiple choice, checkbox, short responses and long respones. Our goal is to try and provide 5 possible career paths based on the user responses to the questions with an explaination about the careers including their salary, possible path to their career, an indeed link and a linkdIn link to potential jobs and potential networking oppurtunities.",
-//       },
-//     ],
-//     model: "gpt-3.5-turbo",
-//   });
-
-//   console.log(completion.choices[0]);
-// }
-// main();
+  console.log(completion.choices[0]);
+}
+main();
