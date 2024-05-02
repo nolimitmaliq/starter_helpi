dc3eefee2fabda2d3cfd8be7b9ed96a791953ff5
rahul5400 <rahulp5400@yahoo.com>
2024-05-02 14:24:37 -0400

    Created a loading screen

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,26 +1,39 @@
 import { Card } from "react-bootstrap";
 import "./Results.css";
-interface careers {
+import { Spinner } from "./Spinner";
+
+interface Careers {
   careers: string[];
 }
-export function Results({ careers }: careers) {
+
+export function Results({ careers }: Careers) {
   return (
     <>
       <div className="Results-Title">Results</div>
       <div className="Results">
-        {careers.slice(0, 5).map((career, index) => (
-          <div key={index} className="career-container">
-            <Card className="career-card">
-              <Card.Body>
-                {/* Optionally, you can add a title for each career */}
-                {/* <Card.Title>Career {index + 1}</Card.Title> */}
-                <Card.Text>{career}</Card.Text>
-              </Card.Body>
-            </Card>
-            {index < 4 && <hr className="career-divider" />}{" "}
-            {/* Add horizontal line between each card */}
-          </div>
-        ))}
+        {careers.length === 0 ? (
+          <Spinner />
+        ) : (
+          careers.map((career, index) => {
+            // Splitting career details into bullet points
+            const careerDetails = career.split("\n");
+            // Extracting career title
+            const title = careerDetails.shift(); // Remove first element from array and return it
+
+            return (
+              <Card key={index} className="career-card">
+                <Card.Title>{title}</Card.Title>
+                <Card.Body>
+                  <ul>
+                    {careerDetails.map((detail, detailIndex) => (
+                      <li key={detailIndex}>{detail}</li>
+                    ))}
+                  </ul>
+                </Card.Body>
+              </Card>
+            );
+          })
+        )}
       </div>
     </>
   );
